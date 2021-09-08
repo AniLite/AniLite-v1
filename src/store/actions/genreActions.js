97ab71057d791ec857205d3fd872f1dc7f1d70ed
Aarush Sinha <81818452+CODElST@users.noreply.github.com>
2021-09-08 18:11:19 +0530

    Removing Proxy URL

--- src/store/actions/genreActions.js
+++ src/store/actions/genreActions.js
@@ -8,7 +8,7 @@ import axios from "axios";
 export const listGenres = () => async (dispatch) => {
   try {
     dispatch({ type: GENRE_LIST_REQUEST });
-    const data = await axios.get("/api/genre/?limit=100");
+    const data = await axios.get("https://anilite-api.herokuapp.com/api/genre/?limit=100");
     dispatch({ type: GENRE_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: GENRE_LIST_FAIL, payload: error.message });
